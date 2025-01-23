038fd509a212eb01e3f3c40f1d812516fe40d5a5
Alumist <codealumist@gmail.com>
2025-01-22 21:57:32 -0800

    fixed root bg

--- app/components/content.tsx
+++ app/components/content.tsx
@@ -2,21 +2,45 @@ import Image from 'next/image';
 
 export default function Content() {
   return (
-    <section className='grid grid-cols-2 gap-8 justify-center items-center text-center w-full h-screen bg-white first'>
+    <section className='grid grid-cols-2 gap-8 justify-center items-center text-center w-full h-screen first'>
       <div className='flex flex-col justify-center items-center'>
-        <Image src='/images/engagement.png' alt='1' width={500} height={500} />
+        <Image
+          src='/images/engagement.png'
+          alt='1'
+          width={500}
+          height={500}
+          className='rounded'
+        />
         <h1 className='text-4xl font-bold'>Engagement</h1>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <Image src='/images/engagement.png' alt='1' width={500} height={500} />
+        <Image
+          src='/images/engagement.png'
+          alt='1'
+          width={500}
+          height={500}
+          className='rounded'
+        />
         <h1 className='text-4xl font-bold'>Engagement</h1>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <Image src='/images/engagement.png' alt='1' width={500} height={500} />
+        <Image
+          src='/images/engagement.png'
+          alt='1'
+          width={500}
+          height={500}
+          className='rounded'
+        />
         <h1 className='text-4xl font-bold'>Engagement</h1>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <Image src='/images/engagement.png' alt='1' width={500} height={500} />
+        <Image
+          src='/images/engagement.png'
+          alt='1'
+          width={500}
+          height={500}
+          className='rounded'
+        />
         <h1 className='text-4xl font-bold'>Engagement</h1>
       </div>
     </section>
