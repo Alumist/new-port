6ab6a17f0e1fb042125f5cdbfbfa7a3c8c7c70d5
Alumist <codealumist@gmail.com>
2025-01-25 21:48:36 -0800

    styling

--- app/components/content.tsx
+++ app/components/content.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Content() {
   return (
     <section className='grid grid-cols-2 gap-8 justify-center items-center text-center w-full h-screen first'>
-      <div className='flex flex-col justify-center items-center'>
+      <Link
+        href='/'
+        className='flex flex-col justify-center items-center transition-all duration-300  hover:scale-125 ease-in-out'
+      >
         <Image
           src='/images/engagement.png'
           alt='1'
@@ -11,38 +15,47 @@ export default function Content() {
           height={500}
           className='rounded'
         />
-        <h1 className='text-4xl font-bold'>Engagement</h1>
-      </div>
-      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-4xl my-4 font-bold'>Engagement</h1>
+      </Link>
+      <Link
+        href='/'
+        className='flex flex-col justify-center items-center transition-all duration-300  hover:scale-125 ease-in-out '
+      >
         <Image
-          src='/images/engagement.png'
+          src='/images/smilemore.png'
           alt='1'
           width={500}
           height={500}
           className='rounded'
         />
-        <h1 className='text-4xl font-bold'>Engagement</h1>
-      </div>
-      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-4xl font-bold my-4 '>SmileMore</h1>
+      </Link>
+      <Link
+        href='/'
+        className='flex flex-col justify-center items-center transition-all duration-300  hover:scale-125 ease-in-out'
+      >
         <Image
-          src='/images/engagement.png'
+          src='/images/HolyGrounds.png'
           alt='1'
           width={500}
           height={500}
           className='rounded'
         />
-        <h1 className='text-4xl font-bold'>Engagement</h1>
-      </div>
-      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-4xl font-bold my-4 '>HolyGrounds</h1>
+      </Link>
+      <Link
+        href='/'
+        className='flex flex-col justify-center items-center transition-all duration-300  hover:scale-125 ease-in-out'
+      >
         <Image
-          src='/images/engagement.png'
+          src='/images/loopstudios.jpg'
           alt='1'
           width={500}
           height={500}
           className='rounded'
         />
-        <h1 className='text-4xl font-bold'>Engagement</h1>
-      </div>
+        <h1 className='text-4xl font-bold my-4 '>LoopStudios</h1>
+      </Link>
     </section>
   );
 }
